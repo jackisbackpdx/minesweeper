a93cca67c31e7818a19f3dd744728b23b6071d16
Jackson Kelley <jackson.kelley.g@gmail.com>
2019-11-11 15:34:34 -0800

    made game change to correct size at start

--- app.js
+++ app.js
@@ -23,11 +23,10 @@ const iterator = [
     row8,
 ];
 
-
 let selectedDropDown = document.querySelector('select');
 let main = document.querySelector('main');
 
-let squareAmount = gameSettings.gameSize * gameSettings.gameSize;
+let squareAmount;
 
 function makeSquares() {
     for (let i = 0; i < squareAmount; i++) {
@@ -37,7 +36,6 @@ function makeSquares() {
 }
 
 let sqauresArray = document.querySelectorAll('div');
-setIds();
 
 function generateBombs() {
     for (let i = 0; i < gameSettings.bombs; i++) {
@@ -48,63 +46,74 @@ function generateBombs() {
     }
 }
 
+let difficultyGlobal;
+
 selectedDropDown.addEventListener('change', function() {
     let difficulty = this.value;
     let difficultyToNumber = parseInt(difficulty);
-    let numberDifficulty = difficultyToNumber;
-    
-    if (numberDifficulty === 1){
+    difficultyGlobal = difficultyToNumber;
+    if (difficultyToNumber === 0){
+        gameSettings.gameSize = 5;
+        squareAmount = gameSettings.gameSize * gameSettings.gameSize;
+        gameSettings.bombs = 3;
+        main.style.width = '280px';
+        main.style.height = '280px'; bbv
+        makeSquares();
+    } else if (difficultyToNumber === 1){
         gameSettings.gameSize = 6;
+        squareAmount = gameSettings.gameSize * gameSettings.gameSize;
+        console.log(gameSettings.gameSize);
         gameSettings.bombs = 4;
         main.style.width = '335px';
         main.style.height = '335px';
-    } else if (numberDifficulty === 2){
+        makeSquares();
+    } else if (difficultyToNumber === 2){
         gameSettings.gameSize = 7;
+        squareAmount = gameSettings.gameSize * gameSettings.gameSize;
+        console.log(gameSettings.gameSize);
         gameSettings.bombs = 5;
         main.style.width = '390px';
         main.style.height = '390px';
+        makeSquares();
     } else {
         gameSettings.gameSize = 8;
+        squareAmount = gameSettings.gameSize * gameSettings.gameSize;
         gameSettings.bombs = 6;
         main.style.width = '445px';
         main.style.height = '445px';
+        makeSquares();
+
     }
-    makeSquares();
     generateBombs();
 }, false);
 
-makeSquares();
-generateBombs();
-
-
-
-
-
-
-
-
 function setIds() {
     for (let i = 0; i < sqauresArray.length; i++) {
-        if (i < 5) {
+        if (i < (gameSettings.gameSize)) {
             sqauresArray[i].id = `row1col${i + 1}`;
             row1.push(sqauresArray[i]);
-        } if (i >= 5 && i < 10) {
-            sqauresArray[i].id = `row2col${i - 4}`;
+        } if (i >= (gameSettings.gameSize) && i < (gameSettings.gameSize * 2)) {
+            sqauresArray[i].id = `row2col${i - ((gameSettings.gameSize) - 1)}`;
             row2.push(sqauresArray[i]);
-        } if (i >= 10 && i < 15) {
-            sqauresArray[i].id = `row3col${i - 9}`;
+        } if (i >= (gameSettings.gameSize * 2) && i < (gameSettings.gameSize * 3)) {
+            sqauresArray[i].id = `row3col${i - ((gameSettings.gameSize * 2) - 1)}`;
             row3.push(sqauresArray[i]);
-        } if (i >= 15 && i < 20) {
-            sqauresArray[i].id = `row4col${i - 14}`;
+        } if (i >= (gameSettings.gameSize * 3) && i < (gameSettings.gameSize * 4)) {
+            sqauresArray[i].id = `row4col${i - ((gameSettings.gameSize * 3) - 1)}`;
             row4.push(sqauresArray[i]);
-        } if (i >= 20 && i < 25) {
-            sqauresArray[i].id = `row5col${i - 19}`;
+        } if (i >= (gameSettings.gameSize * 4) && i < (gameSettings.gameSize * 5)) {
+            sqauresArray[i].id = `row5col${i - ((gameSettings.gameSize * 4) - 1)}`;
             row5.push(sqauresArray[i]);
+        } if (i >= (gameSettings.gameSize * 5) && i < (gameSettings.gameSize * 6)) {
+            sqauresArray[i].id = `row6col${i - ((gameSettings.gameSize * 5) - 1)}`;
+            row6.push(sqauresArray[i]);
+        } if (i >= (gameSettings.gameSize * 6) && i < (gameSettings.gameSize * 7)) {
+            sqauresArray[i].id = `row7col${i - ((gameSettings.gameSize * 6) - 1)}`;
+            row7.push(sqauresArray[i]);
+        } if (i >= (gameSettings.gameSize * 7) && i < (gameSettings.gameSize * 8)) {
+            sqauresArray[i].id = `row8col${i - ((gameSettings.gameSize * 7) - 1)}`;
+            row8.push(sqauresArray[i]);
         }
     }
 }
-
-
-
-
-
+setIds();
